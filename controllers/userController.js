9e0af028a1928b276e556687feab45c9fd921189
nasemul1 <nasemul.me@gmail.com>
2025-04-02 21:12:15 +0600

    added signin functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
-import userModel from "../models/userModel.js";
-import { hashPassword } from "../utils/password_encrypt.js";
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { hashPassword, verifyPassword } from "../utils/password_encrypt.js";
 
 const signup = async (req, res, next)=>{
     try {
         
-        const { name, email, password, role } = req.body;
+        const { name, email, password } = req.body;
         const hashedPassword = await hashPassword(password);
-        const newUser = new userModel({
+        const newUser = new User({
             name,
             email,
             password: hashedPassword,
@@ -18,8 +19,51 @@ const signup = async (req, res, next)=>{
         res.json({code: 201, staus: true, message: "User created successfully"});
 
     } catch (error) {
-        next(error);
+        res.json({code: 500, status: false, message: "Internal server error"});
     }
 }
 
-export { signup };
+const signin = async (req, res, next) => {
+    
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({code: 400, status: false, message: "Please provide email and password"});
+    }
+
+    try {
+        
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({code: 404, status: false, message: "User not found"});
+        }
+
+        const isValid = await verifyPassword(password, user.password);
+        if(!isValid){
+            return res.status(401).json({code: 401, status: false, message: "Invalid credentials"});
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({id: user._id, role: user.role, email: user.email}, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+        });
+
+        // Send the token and user data in the response
+        res.status(200).json({
+            status: true,
+            message: 'Signin successful',
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                role: user.role,
+            }
+        })
+
+    } catch (error) {
+        res.json({code: 500, status: false, message: "Internal server error"});
+    }
+}
+
+export { signup, signin };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/userController.js'
+import { signup, signin } from '../controllers/userController.js'
 import validateUser from '../middlewares/validator.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/signup', validateUser, signup);
+userRouter.post('/signin', validateUser, signin);
 
 export default userRouter;
